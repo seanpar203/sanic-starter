88a47d472894b34b89c56a53d517699cb015eac4
Sean Parsons <seanpatrick2013@gmail.com>
2016-12-23 10:20:25 -0500

    Moved sqlalchemy into database.py file.

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from manager import Manager
 
 # App
 from app import create_app
+from models import Base
 
 # Constants.
 manager = Manager()
@@ -15,6 +16,8 @@ def run():
     """ Starts server on port 8000. """
     create_app()
 
+def init_db():
+    Base.metadata.create_all()
 
 if __name__ == '__main__':
     manager.main()
