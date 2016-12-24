8dd161244cf840d2cebc2f27e108ad3b80ef8f06
Sean Parsons <seanpatrick2013@gmail.com>
2016-12-24 12:16:28 -0500

    Registered blueprint and set workers to cpu_count().

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
+import os
 from sanic import Sanic
+
+# Prepend '/api' before every route.
+BP_OPTIONS = {'url_prefix': '/api'}
 
 
 def create_app():
     """ Function for bootstrapping sanic app. """
     app = Sanic(__name__)
-    app.run(host='0.0.0.0', port='8000', debug=True)
+
+    # Register blueprints.
+    from app.controllers.users import user
+    app.blueprint(user, **BP_OPTIONS)
+
+    print(app.router)
+    app.run(host='0.0.0.0', port=8000, debug=True, workers=os.cpu_count())
