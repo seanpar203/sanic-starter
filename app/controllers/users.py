91a9052c68dce1fc43d4202431724b58ec6c2a4c
Sean Parsons <seanpatrick2013@gmail.com>
2016-12-24 21:56:45 -0500

    Added simple insert statement on user blueprint route.

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,9 +1,17 @@
-from sanic.response import json
 from sanic import Blueprint
+from sanic.response import json
+from sqlalchemy import insert
+
+from app.database import db
+from app.models.users import User
 
 user = Blueprint('user')
 
 
 @user.route('/', methods=['GET'])
 async def foo(request):
-    return json({'msg': 'hi from blueprint'})
+    stmt = insert(User).values(email='some@gmail.com')
+    conn = db.connect()
+    conn.execute(stmt)
+    conn.close()
+    return json({'msg': 'Successfully created a user.'})
