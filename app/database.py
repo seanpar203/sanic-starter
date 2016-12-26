58e6d4f4d10a32858511e21d0c338f09050340cd
Sean Parsons <seanpatrick2013@gmail.com>
2016-12-26 13:59:33 -0500

    Created scoped_session context manager to follow best practices.

--- app/database.py
+++ app/database.py
@@ -1,11 +1,31 @@
 """ This module exports the database engine.
 
 Notes:
-     This makes importing the db anywhere in the app easy.
-     Aswell as grabbing a connection from the pool with
-     `db.connect()`.
+     Using the scoped_session contextmanager is
+     best practice to ensure the session gets closed
+     and reduces noise in code by not having to manually
+     commit or rollback the db if a exception occurs.
 """
 import os
-from sqlalchemy import create_engine
+from contextlib import contextmanager
 
-db = create_engine(os.environ['DATABASE_URL'])
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(os.environ['DATABASE_URL'])
+
+# Session to be used throughout app.
+Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def scoped_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
